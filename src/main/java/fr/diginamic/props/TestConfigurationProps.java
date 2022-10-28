7c358d912b97df25e1ed5980684623f032819185
Marine Desmier <desmier.marine@bbox.fr>
2022-10-28 17:45:31 +0200

    suite tp

--- src/main/java/fr/diginamic/props/TestConfigurationProps.java
+++ src/main/java/fr/diginamic/props/TestConfigurationProps.java
@@ -1,10 +1,25 @@
 package fr.diginamic.props;
 
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
 public class TestConfigurationProps {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ResourceBundle config = ResourceBundle.getBundle("database");
+		String driverName = config.getString("database.driver");
+		System.out.println(driverName);
+		
+		config.getKeys();
+		
+		Enumeration<String> monEnum = config.getKeys();
+		while (monEnum.hasMoreElements()) {
+			String cle = monEnum.nextElement();
+			String val = config.getString(cle);
+			System.out.println(cle + " -> " + val);
+		}
+		
 	}
 
 }
